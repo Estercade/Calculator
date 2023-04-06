37b27fd5dee97c351cf3f16499f829b78b3c15bf
Estercade <withrwithoutyou@gmail.com>
2023-04-05 21:16:25 -0400

    Create basic operate and operation functions

---
+++ js/javascript.js
@@ -0,0 +1,26 @@
+function operate(operator, num1, num2) {
+    if (operator === '+') {
+        add(num1, num2);
+    }
+    else if (operator === '-') {
+        subtract(num1, num2);
+    }
+    else if (operator === '*') {
+        multiply(num1, num2);
+    }
+    else if (operator === '/') {
+        divide(num1, num2);
+    }
+}
+
+function add(num1, num2) {
+    return;
+}
+
+function multiple(num1, num2) {
+    return;
+}
+
+function divide(num1, num2) {
+    return;
+}
