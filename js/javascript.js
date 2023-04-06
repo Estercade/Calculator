a7d51d5a100fdff757d616507cb4a7a4acf9bc82
Estercade <withrwithoutyou@gmail.com>
2023-04-05 23:42:46 -0400

    Add math functions and button event listeners

--- js/javascript.js
+++ js/javascript.js
@@ -1,26 +1,94 @@
 function operate(operator, num1, num2) {
     if (operator === '+') {
-        add(num1, num2);
+        return add(num1, num2);
     }
     else if (operator === '-') {
-        subtract(num1, num2);
+        return subtract(num1, num2);
     }
     else if (operator === '*') {
-        multiply(num1, num2);
+        return multiply(num1, num2);
     }
     else if (operator === '/') {
-        divide(num1, num2);
+        return divide(num1, num2);
     }
 }
 
 function add(num1, num2) {
-    return;
+    return (Number(num1) + Number(num2));
 }
 
-function multiple(num1, num2) {
-    return;
+function subtract(num1, num2) {
+    return (Number(num1) - Number(num2));
+}
+
+function multiply(num1, num2) {
+    return (Number(num1) * Number(num2));
 }
 
 function divide(num1, num2) {
-    return;
+    return (Number(num1) / Number(num2));
+}
+
+let num1, num2, operator;
+
+const display = document.getElementById('display');
+
+// const buttonsList = document.querySelectorAll('button');
+// buttonsList.forEach((button) => button.addEventListener('click', logButton));
+
+var currentValue = 0;
+
+const equalBtn = document.getElementById('equal');
+equalBtn.addEventListener('click', (e) => {
+    num2 = currentValue;
+    currentValue = operate(operator, num1, num2);
+    updateDisplay();
+    num1 = undefined;
+    num2 = undefined;
+    operator = undefined;
+});
+
+const operatorBtnList = document.querySelectorAll('.operatorBtn');
+operatorBtnList.forEach((operatorBtn) => operatorBtn.addEventListener('click', (e) => {
+    operator = e.target.innerHTML;
+    if (num1 === undefined) {
+        num1 = currentValue;
+        currentValue = 0;
+        updateDisplay();
+    } else if (num1 != undefined && operator != undefined) {
+        num2 = currentValue;
+        currentValue = operate(operator, num1, num2);
+        num1 = currentValue;
+        currentValue = 0;
+        updateDisplay();
+        num2 = undefined;
+        operator = undefined;
+}}));
+
+const numberBtnList = document.querySelectorAll('.numberBtn');
+numberBtnList.forEach((numberBtn) => numberBtn.addEventListener('click', (e) => {
+    currentValue === undefined || currentValue === 0 ? currentValue = e.target.innerHTML : currentValue += e.target.innerHTML;
+    updateDisplay();
+}));
+
+// Update display with every button press
+// buttonsList.forEach((button) => button.addEventListener('click', updateDisplay));
+
+// Initial display content
+bottomDisplay.textContent = '123456789';
+
+function updateDisplay() {
+    topDisplay.textContent = `${num1} ${operator} ${num2}`
+    bottomDisplay.textContent = `${currentValue}`;
+}
+
+const clearButton = document.getElementById('clearBtn');
+clearButton.addEventListener('click', clear);
+
+function clear() {
+    num1 = undefined;
+    num2 = undefined;
+    currentValue = 0;
+    operator = undefined;
+    updateDisplay();
 }
